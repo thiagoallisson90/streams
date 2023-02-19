26540b2dedb594955a78424ad0a52d1efc8b7cd4
Thiago Allisson <thiago.allisson@ufpi.edu.br>
2023-02-19 14:13:23 -0300

    streams no módulo HTTP

--- stream-server.js
+++ stream-server.js
@@ -1,7 +1,25 @@
 import http from 'node:http';
+import { Writable, Transform } from 'node:stream';
+
+class InverseNumberStream extends Transform {
+  _transform(chunk, enconding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
+    console.log(transformed);
+    callback(null, Buffer.from(String(transformed)));
+  }
+}
+
+class MultipleByTen extends Writable {
+  _write(chunk, enconding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback();
+  }
+}
 
 const server = http.createServer((req, res) => {
-  
+  return req
+    .pipe(new InverseNumberStream())
+    .pipe(res);
 });
 
 server.listen(3334);
